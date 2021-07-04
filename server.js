c0dee3c0345179d54328b61b384f47256371ded3
Karanburnwal <48697249+Karanburnwal@users.noreply.github.com>
2021-07-04 20:03:59 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express=require('express')
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@cluster0.p32px.mongodb.net/emaildb` ,{useNewUrlParser:true, useUnifiedTopology: true}).catch(err => console.log(err)) ;
+const db = mongoose.connection;
+db.once('open', () => console.log('Successfully connected to MongoDB'));
+db.on('error', (e) => console.log(e));
+
+const emailSchema = new mongoose.Schema({
+    emailid:String
+})
+const Email = mongoose.model('Email',emailSchema)
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/Home.html');
+})
+app.post('/submit',function(req,res){
+    console.log(req.body.email);
+    const email = new Email({
+        emailid:req.body.email
+    })
+    email.save();
+    res.sendFile(__dirname+'/submit.html')
+})
+
+
+// const port = process.env.PORT;
+// if(port ==null || port==""){
+//     port=3000;
+// }
+app.listen(3000,function(){
+    console.log('server running at 3000');
+})
